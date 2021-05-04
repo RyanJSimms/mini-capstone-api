29cc77228147bf6f6655bed01e3b72f0de385729
Ryan Simms <simmsey53@gmail.com>
2021-05-04 19:47:55 -0400

    Added product controller with a couple products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Coffee Mug", price: 8, image_url: "testurl", description: "A wonderful mug for consuming coffee or any other liquid!")
+Product.create(name: "Glasses", price: 35, image_url: "testurl", description: "Ever have trouble seeing things? Then order you a brand new pair of state of the art glasses!")
