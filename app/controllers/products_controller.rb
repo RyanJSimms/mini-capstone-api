813d213666145ac7b2aebaec0fdeeb28e6b4747f
Ryan Simms <simmsey53@gmail.com>
2021-05-04 21:04:57 -0400

    Finalized all information with routes and methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,5 +4,28 @@ class ProductsController < ApplicationController
     render json: products.as_json
   end
 
-  
+  def select_product_method_1
+    product = Product.first
+    render json: product.as_json
+  end
+
+  def select_product_method_2
+    product = Product.second
+    render json: product.as_json
+  end
+
+  def select_product_method_3
+    product = Product.third
+    render json: product.as_json
+  end
+
+  def select_product_method_4
+    product = Product.fourth
+    render json: product.as_json
+  end
+
+  def select_product_method_5
+    product = Product.fifth
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/products", controller: "products", action: "all_products_method"
+  get "/select_product_1", controller: "products", action: "select_product_method_1"
+  get "/select_product_2", controller: "products", action: "select_product_method_2"
+  get "/select_product_3", controller: "products", action: "select_product_method_3"
+  get "/select_product_4", controller: "products", action: "select_product_method_4"
+  get "/select_product_5", controller: "products", action: "select_product_method_5"
 end
